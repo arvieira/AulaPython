681ee2940aee3a675b189ca17a485fce92baabf7
Andre <andreribeirovieira@gmail.com>
2019-01-30 09:38:21 -0200

    Teste

--- Aula3Exercicio.py
+++ Aula3Exercicio.py
@@ -2,10 +2,10 @@ import requests
 import json
 from collections import Counter
 
-
 texto = requests.get("https://bit.ly/2Cu73N6").text
 print ("Texto Original")
 print (texto)
+print ("\n")
 
 # Uma string é uma lista de caracteres. Logo, se eu usar o contador diretamente na string
 # terei um problema de que ele vai contar letra por letra e não palavras. Então, preciso
@@ -14,3 +14,13 @@ print (texto)
 words = texto.split()
 contador = Counter(words)
 print (contador)
+print ("\n")
+print (contador.most_common(10))
+print ("\n")
+
+# Por padrão o separador do split é o espaço ou o tab. Eu posso mudar esse delimitador.
+exemplo = "Teste | BLBL | nada"
+lista_exemplo = exemplo.split(" | ")
+print (lista_exemplo)
+print ("\n")
+
