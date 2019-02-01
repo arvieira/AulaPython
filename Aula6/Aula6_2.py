c6928fac53bfd63299d8694487e39ef274706628
Andre <andreribeirovieira@gmail.com>
2019-02-01 12:05:48 -0200

    Teste

--- Aula6/Aula6_2.py
+++ Aula6/Aula6_2.py
@@ -35,54 +35,57 @@ class Retangulo:
 	def diagonal (self):
 		return math.sqrt(self.largura ** 2 + self.altura ** 2)
 
+# Métodos estáticos não aqueles que apenas operam em variáveis estáticas da classe
+# Não possui uma palavra reservada para isso, mas eu devo chamar pelo nome da classe
+
 # A seguir temos uma classe quadrado que extende (herda) do retângulo
 # mas ele redefine o construtor, de modo a colocar os lados iguais
 class Quadrado (Retangulo):
-	contador = 0
+	contador = 0			# Preciso redeclarar o contador, pq senão ele compartilha as estáticas da classe pai
 
-	def __init__ (self, lado):
-		self.largura = lado
-		self.altura = lado
+	def __init__ (self, lado):	# O mesmo compartilhamento ocorre com o construtor que herdará a inicialização das
+		self.largura = lado	# variáveis do retangulo. Desse modo, eu preciso redeclarar as variáveis estáticas
+		self.altura = lado	# e os construtores de classe para não ter esse compartilhamento.
 		Quadrado.contador += 1
 
 
 
 
-ret = Retangulo (3, 4)
+ret1 = Retangulo (3, 4)
 print ("Retangulo")
-print (ret.area())
-print (ret.diagonal(), "\n")
+print (ret1.area())
+print (ret1.diagonal(), "\n")
 
-
-ret = Retangulo (4, 5)
+ret2 = Retangulo (4, 5)
 print ("Retangulo")
-print (ret.area())
-print (ret.diagonal(), "\n")
+print (ret2.diagonal())
+ret2.altura = 9
+print (ret2.diagonal(), "\n")
 
-quad = Quadrado (4)
+quad1 = Quadrado (4)
 print ("Quadrado")
-print (quad.area())
-print (quad.diagonal(), "\n")
+print (quad1.area())
+print (quad1.diagonal(), "\n")
 
-quad = Quadrado (2)
+quad2 = Quadrado (2)
 print ("Quadrado")
-print (quad.area())
-print (quad.diagonal(), "\n")
+print (quad2.area())
+print (quad2.diagonal(), "\n")
 
-quad = Quadrado (3)
+quad3 = Quadrado (3)
 print ("Quadrado")
-print (quad.area())
-print (quad.diagonal(), "\n")
+print (quad3.area())
+print (quad3.diagonal(), "\n")
 
-ret = Retangulo (4, 5)
+ret3 = Retangulo (4, 5)
 print ("Retangulo")
-print (ret.area())
-print (ret.diagonal(), "\n")
+print (ret3.area())
+print (ret3.diagonal(), "\n")
 
-quad = Quadrado (3)
+quad4 = Quadrado (3)
 print ("Quadrado")
-print (quad.area())
-print (quad.diagonal(), "\n")
+print (quad4.area())
+print (quad4.diagonal(), "\n")
 
 print ("Total Retangulo", Retangulo.contador)
 print ("Total Quadrado", Quadrado.contador)
